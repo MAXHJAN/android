3ff972cceb951869d361dcb1f79fa12bc8d5f3e8
mx <maxhjan@gmial.com>
2016-04-05 12:01:02 +0800

    C:/Program Files/Git

---
+++ PhoneBook/src/max/phonebook/Adapter/SortAdapter.java
@@ -0,0 +1,139 @@
+package max.phonebook.Adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+import max.phonebook.R;
+import max.phonebook.Ben.Person;
+
+public class SortAdapter extends BaseAdapter implements SectionIndexer{
+	private List<Person> list = null;
+	private Context mContext;
+	
+	public SortAdapter(Context mContext, List<Person> list) {
+		this.mContext = mContext;
+		this.list = list;
+	}
+	
+	/**
+	 * 当ListView数据发生变化时,调用此方法来更新ListView
+	 * @param list
+	 */
+	public void updateListView(List<Person> list){
+		this.list = list;
+		notifyDataSetChanged();
+	}
+
+	public int getCount() {
+		return this.list.size();
+	}
+
+	public Object getItem(int position) {
+		return list.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(final int position, View view, ViewGroup arg2) {
+		ViewHolder viewHolder = null;
+		final Person mContent = list.get(position);
+		if (view == null) {
+			viewHolder = new ViewHolder();
+			view = LayoutInflater.from(mContext).inflate(R.layout.phonelistview_item, null);
+			viewHolder.Head=(ImageView) view.findViewById(R.id.head);
+			viewHolder.Name=(TextView) view.findViewById(R.id.name);
+			viewHolder.PhoneNumber=(TextView) view.findViewById(R.id.phone);
+			viewHolder.tvLetter = (TextView) view.findViewById(R.id.catalog);
+			view.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) view.getTag();
+		}
+		
+		//根据position获取分类的首字母的Char ascii值
+		int section = getSectionForPosition(position);
+		
+		//如果当前位置等于该分类首字母的Char的位置 ，则认为是第一次出现
+		if(position == getPositionForSection(section)){
+			viewHolder.tvLetter.setVisibility(View.VISIBLE);
+			viewHolder.tvLetter.setText(mContent.getSortLetters());
+		}else{
+			viewHolder.tvLetter.setVisibility(View.GONE);
+		}
+		viewHolder.Head.setImageResource(R.drawable.head);
+		viewHolder.Name.setText(this.list.get(position).getName());
+		viewHolder.PhoneNumber.setText(this.list.get(position).getPhonenumber());
+		viewHolder.tvLetter.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		return view;
+
+	}
+	
+
+
+	class ViewHolder{
+		TextView tvLetter;
+		 ImageView Head;//头像
+		 TextView Name;// 姓名
+		TextView PhoneNumber;// 电话
+	}
+
+
+	/**
+	 * 根据ListView的当前位置获取分类的首字母的Char ascii值
+	 */
+	public int getSectionForPosition(int position) {
+		return list.get(position).getSortLetters().charAt(0);
+	}
+
+	/**
+	 * 根据分类的首字母的Char ascii值获取其第一次出现该首字母的位置
+	 */
+	public int getPositionForSection(int section) {
+		for (int i = 0; i < getCount(); i++) {
+			String sortStr = list.get(i).getSortLetters();
+			char firstChar = sortStr.toUpperCase().charAt(0);
+			if (firstChar == section) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * 提取英文的首字母，非英文字母用#代替。
+	 * 
+	 * @param str
+	 * @return
+	 */
+	private String getAlpha(String str) {
+		String  sortStr = str.trim().substring(0, 1).toUpperCase();
+		// 正则表达式，判断首字母是否是英文字母
+		if (sortStr.matches("[A-Z]")) {
+			return sortStr;
+		} else {
+			return "#";
+		}
+	}
+
+	@Override
+	public Object[] getSections() {
+		return null;
+	}
+}
---
+++ PhoneBook/src/max/phonebook/Fragment/ContactsFragment.java
@@ -0,0 +1,190 @@
+package max.phonebook.Fragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import max.phonebook.MyAppLication;
+import max.phonebook.PersonInfoActivity;
+import max.phonebook.R;
+import max.phonebook.Adapter.SortAdapter;
+import max.phonebook.Ben.CharacterParser;
+import max.phonebook.Ben.Person;
+import max.phonebook.Ben.PinyinComparator;
+import max.phonebook.ContentResolver.GetPhoneInfo;
+import max.phonebook.View.ClearEditText;
+import max.phonebook.View.SideBar;
+import max.phonebook.View.SideBar.OnTouchingLetterChangedListener;
+
+public class ContactsFragment extends Fragment {
+
+	private View mview;
+	private ListView sortListView;
+	private SortAdapter mAdapter;
+	private SideBar sideBar;
+	private TextView dialog;
+	private ClearEditText mClearEditText;
+	
+	private PinyinComparator pinyinComparator;
+	//获取汉字的首字母
+	private CharacterParser characterParser;
+	private ArrayList<Person> PhoneInfos=new ArrayList<Person>();//创建一个保存获得的联系人的列表来存放数据
+	
+	private  GetPhoneInfo gpi;//创建一个获得联系人
+	
+	private static final int GET_PHONE_OK=0x01;//获取联系人成功
+	private MyAppLication MyApp;
+	public FragmentActivity activity;		
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		if (mview == null)
+	    {
+			mview = inflater.inflate(R.layout.contactsfragment, null);
+	    }
+	    // 缓存的rootView需要判断是否已经被加过parent，如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+	    ViewGroup parent = (ViewGroup) mview.getParent();
+	    if (parent != null)
+	    {
+	      parent.removeView(mview);
+	    }
+	    activity=getActivity();
+	    gpi=new GetPhoneInfo(activity);
+	    pinyinComparator = new PinyinComparator();
+	    characterParser = CharacterParser.getInstance();
+	    GetPhoneInfos();
+	    intView(mview);
+	    SetView();
+	    OnClickLisenler();
+		return mview;
+	}
+
+	
+	@SuppressWarnings("static-access")
+	public void GetPhoneInfos()
+	{
+		      MyApp=(MyAppLication) activity.getApplication();						
+				PhoneInfos.clear();
+				PhoneInfos=MyApp.getPhoneInfos();				
+				Log.e("123", "--------------"+PhoneInfos.size());					 
+	}
+	
+	/**
+	 * 初始化控件
+	 * @param view
+	 */
+	private void intView(View view)
+	{
+		
+		sideBar = (SideBar) view.findViewById(R.id.sidrbar);
+		dialog = (TextView)view. findViewById(R.id.dialog);
+		sortListView=(ListView) view.findViewById(R.id.listView);
+		mClearEditText = (ClearEditText)view. findViewById(R.id.filter_edit);
+		sideBar.setTextView(dialog);
+	}
+	
+	/**
+	 * 相关组件监听
+	 */
+	public void OnClickLisenler()
+	{
+		//设置右侧触摸监听
+			sideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener() {
+					
+					@Override
+					public void onTouchingLetterChanged(String s) {
+						//该字母首次出现的位置
+						int position = mAdapter.getPositionForSection(s.charAt(0));
+						if(position != -1){
+							sortListView.setSelection(position);
+						}
+						
+					}
+				});
+				//listview监听
+			sortListView.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						//这里要利用adapter.getItem(position)来获取当前position所对应的对象
+						Bundle mBundle = new Bundle(); 
+						mBundle.putSerializable("person", PhoneInfos.get(position));
+						Intent in=new Intent(activity, PersonInfoActivity.class);
+						in.putExtras(mBundle);  
+						startActivity(in);
+					}
+				});
+			
+			
+			//根据输入框输入值的改变来过滤搜索
+			mClearEditText.addTextChangedListener(new TextWatcher() {
+				
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count) {
+					//当输入框里面的值为空，更新为原来的列表，否则为过滤数据列表
+					mAdapter.updateListView(filterData(s.toString()));
+				}
+				
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count,
+						int after) {
+					
+				}
+				
+				@Override
+				public void afterTextChanged(Editable s) {
+				}
+			});
+	}
+	/**
+	 * 适配数据
+	 */
+	public void SetView()
+	{
+		Collections.sort(PhoneInfos, pinyinComparator);
+		mAdapter=new SortAdapter(activity, PhoneInfos);
+		sortListView.setAdapter(mAdapter);
+	}
+	
+	
+	
+	/**
+	 * 根据输入框中的值来过滤数据并更新ListView
+	 * @param filterStr
+	 */
+	public List<Person> filterData(String filterStr){
+		List<Person> filterDateList = new ArrayList<Person>();		
+		if(TextUtils.isEmpty(filterStr)){
+			filterDateList = PhoneInfos;
+		}else{
+			filterDateList.clear();
+			for(Person person : PhoneInfos){
+				String name = person.getName();
+				if(name.indexOf(filterStr.toString()) != -1 || characterParser.getSelling(name).startsWith(filterStr.toString()))
+				{
+					filterDateList.add(person);
+				}
+			}
+		}
+		// 根据a-z进行排序
+		Collections.sort(filterDateList, pinyinComparator);	
+		return filterDateList;
+	}
+
+}
